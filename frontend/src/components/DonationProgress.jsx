55f38280e7ca6b445a3b0b5b51b7c91ba5296262
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2024-11-12 22:38:50 +0530

    🎨 DonationProgress from backend

--- frontend/src/components/DonationProgress.jsx
+++ frontend/src/components/DonationProgress.jsx
@@ -2,24 +2,71 @@ import { Progress, Typography } from "antd";
 const { Title } = Typography;
 import { Col, Row } from "antd";
 import DonationPurchaseables from "./DonationPurchaseables";
+import { useEffect, useState } from "react";
+import Constants from "../utils/Constants";
 
 export default function DonationProgress() {
-	const totalDonation = 10000;
-	const boostAmount = 2000;
-	const currentDonation = boostAmount + 0;
+	const [donationData, setDonationData] = useState();
+
+	useEffect(() => {
+		fetchDonationData();
+		setInterval(() => {
+			fetchDonationData();
+		}, 5000);
+	}, []);
+
+	const fetchDonationData = async () => {
+		const response = await fetch(`${Constants.BACKEND_SERVER_ROOT}/donations/1`);
+		// if response is 404, create a new entity
+		if (response.status === 404) {
+			const newlyCreatedResponseData = await createDefaultEntity();
+			setDonationData(newlyCreatedResponseData);
+		} else {
+			const data = await response.json();
+			setDonationData(data);
+		}
+	};
+
+	const createDefaultEntity = async () => {
+		const response = await fetch(`${Constants.BACKEND_SERVER_ROOT}/donations/1`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				id: 1,
+				totalDonation: 10000,
+				boostAmount: 0,
+				currentDonation: 0,
+			}),
+		});
+		return response.json();
+	};
+
 	return (
 		<>
 			<Row justify="space-evenly" gutter={[4, 20]}>
 				<Col>
-					<Title>Donation Goal: {`₹${currentDonation} / ₹${totalDonation}`}</Title>
+					<Title>
+						Donation Goal:{" "}
+						{`₹${donationData?.currentDonation + donationData?.boostAmount} / ₹${
+							donationData?.totalDonation
+						}`}
+					</Title>
 				</Col>
 				<Col xs={22}>
-					<DonationPurchaseables donatedAmount={currentDonation} />
+					<DonationPurchaseables
+						donatedAmount={donationData?.currentDonation + donationData?.boostAmount}
+					/>
 				</Col>
 				<Col xs={22}>
 					<Progress
 						size={{ height: 50 }}
-						percent={(currentDonation / totalDonation) * 100}
+						percent={
+							((donationData?.currentDonation + donationData?.boostAmount) /
+								donationData?.totalDonation) *
+							100
+						}
 						percentPosition={{
 							align: "center",
 							type: "inner",
---
+++ frontend/src/utils/Constants.js
@@ -0,0 +1,7 @@
+const BASE_PATH = import.meta.env.VITE_BASE_PATH || "/donation-board/";
+const BACKEND_SERVER_ROOT = import.meta.env.VITE_BACKEND_SERVER_ROOT || "http://localhost:8080";
+
+export default {
+	BASE_PATH,
+	BACKEND_SERVER_ROOT,
+};
