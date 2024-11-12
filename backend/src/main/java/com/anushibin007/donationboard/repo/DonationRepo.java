50df331b673e93224ab0589b280a47d1c3f5c361
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2024-11-12 21:46:30 +0530

    🎨 CrudRepository instead of PagingAndSortingRepository

--- backend/src/main/java/com/anushibin007/donationboard/repo/DonationRepo.java
+++ backend/src/main/java/com/anushibin007/donationboard/repo/DonationRepo.java
@@ -1,11 +1,11 @@
 package com.anushibin007.donationboard.repo;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.anushibin007.donationboard.model.DonationData;
 
 @RepositoryRestResource(collectionResourceRel = "donations", path = "donations")
-public interface DonationRepo extends PagingAndSortingRepository<DonationData, Integer> {
+public interface DonationRepo extends CrudRepository<DonationData, Integer> {
 
 }
--- backend/src/main/java/com/anushibin007/donationboard/repo/VisitorCountRepo.java
+++ backend/src/main/java/com/anushibin007/donationboard/repo/VisitorCountRepo.java
@@ -1,11 +1,11 @@
 package com.anushibin007.donationboard.repo;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.anushibin007.donationboard.model.VisitorCount;
 
 @RepositoryRestResource(collectionResourceRel = "visitorcount", path = "visitorcount")
-public interface VisitorCountRepo extends PagingAndSortingRepository<VisitorCount, Integer> {
+public interface VisitorCountRepo extends CrudRepository<VisitorCount, Integer> {
 
 }
