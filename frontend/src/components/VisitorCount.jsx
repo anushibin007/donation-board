d9cf9eb6704346f93f827b1ad180fb767d26ad24
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-11-13 14:11:21 +0530

    🎨 Fetch visitorCount from backend

--- frontend/src/components/VisitorCount.jsx
+++ frontend/src/components/VisitorCount.jsx
@@ -1,8 +1,52 @@
 import { Col, Row, Typography } from "antd";
+import { useEffect, useState } from "react";
+import Constants from "../utils/Constants";
 const { Text } = Typography;
 
 export default function VisitorCount() {
-	const visitorCount = 100;
+	const [visitorCount, setVisitorCount] = useState(50);
+
+	useEffect(() => {
+		fetchVisitorCount();
+	}, []);
+
+	const fetchVisitorCount = async () => {
+		const response = await fetch(`${Constants.BACKEND_SERVER_ROOT}/visitorcount/1`);
+		// if response is 404, create a new entity
+		if (response.status === 404) {
+			const newlyCreatedResponseData = await updateCount();
+			setVisitorCount(newlyCreatedResponseData?.count);
+		} else {
+			const data = await response.json();
+			setVisitorCount(data?.count);
+			if (data) {
+				updateCount({
+					id: 1,
+					count: data?.count + 1,
+				});
+			}
+		}
+	};
+
+	const updateCount = async (countData) => {
+		if (!countData) {
+			countData = {
+				id: 1,
+				count: 0,
+			};
+		}
+
+		const response = await fetch(`${Constants.BACKEND_SERVER_ROOT}/visitorcount/1`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(countData),
+		});
+		return response.json();
+	};
+
+	// const visitorCount = 100;
 
 	const luckyWinner = () => {
 		if (visitorCount % 100 === 0) {
