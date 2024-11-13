076448b5d03072e89cf08fa4165bae391f457264
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-11-13 12:35:56 +0530

    ✨ Config page

---
+++ frontend/src/components/Config.jsx
@@ -0,0 +1,124 @@
+import { Button, Form, InputNumber } from "antd";
+import { useEffect, useState } from "react";
+import Constants from "../utils/Constants";
+
+export default function Config() {
+	const [form] = Form.useForm();
+	const [donationData, setDonationData] = useState();
+
+	useEffect(() => {
+		fetchDonationData();
+	}, []);
+
+	// Update form fields when donationData changes
+	useEffect(() => {
+		if (donationData) {
+			form.setFieldsValue(donationData);
+		}
+	}, [donationData, form]);
+
+	const fetchDonationData = async () => {
+		const response = await fetch(`${Constants.BACKEND_SERVER_ROOT}/donations/1`);
+		// if response is 404, create a new entity
+		if (response.status === 404) {
+			const newlyCreatedResponseData = await updateData();
+			setDonationData(newlyCreatedResponseData);
+		} else {
+			const data = await response.json();
+			setDonationData(data);
+			console.log(data);
+		}
+	};
+
+	const updateData = async (updatedData) => {
+		if (!updatedData) {
+			updatedData = {
+				id: 1,
+				totalDonation: 10000,
+				boostAmount: 0,
+				currentDonation: 0,
+			};
+		}
+
+		const response = await fetch(`${Constants.BACKEND_SERVER_ROOT}/donations/1`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedData),
+		});
+		return response.json();
+	};
+
+	const onFinish = (values) => {
+		console.log("Success:", values);
+		updateData({ id: 1, ...values });
+	};
+	const onFinishFailed = (errorInfo) => {
+		console.log("Failed:", errorInfo);
+	};
+
+	return (
+		<>
+			<div>Config</div>
+			<Form
+				form={form}
+				name="basic"
+				labelCol={{
+					span: 8,
+				}}
+				wrapperCol={{
+					span: 16,
+				}}
+				style={{
+					maxWidth: 600,
+				}}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+				autoComplete="off"
+			>
+				<Form.Item
+					label="totalDonation"
+					name="totalDonation"
+					rules={[
+						{
+							required: true,
+							message: "Please input totalDonation!",
+						},
+					]}
+				>
+					<InputNumber />
+				</Form.Item>
+				<Form.Item
+					label="boostAmount"
+					name="boostAmount"
+					rules={[
+						{
+							required: true,
+							message: "Please input boostAmount!",
+						},
+					]}
+				>
+					<InputNumber />
+				</Form.Item>
+				<Form.Item
+					label="currentDonation"
+					name="currentDonation"
+					rules={[
+						{
+							required: true,
+							message: "Please input currentDonation!",
+						},
+					]}
+				>
+					<InputNumber />
+				</Form.Item>
+				<Form.Item label={null}>
+					<Button type="primary" htmlType="submit">
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
+	);
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Constants from "./utils/Constants.js";
+import Config from "./components/Config.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<BrowserRouter basename={Constants.BASE_PATH}>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/config" element={<Config />} />
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>
 );
